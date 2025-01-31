9ab8fd8a0c9060d98157650645063346e14b8102
HaoHV2k5 <magicmath2k5@gmail.com>
2025-01-31 22:10:36 +0700

    init

---
+++ Laptopshop/src/test/java/vn/Laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.Laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
