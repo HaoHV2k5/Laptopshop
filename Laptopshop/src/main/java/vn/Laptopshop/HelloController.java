ca59fbbc66dac83a03205ae3854663422db3a4b5
HaoHV2k5 <magicmath2k5@gmail.com>
2025-02-07 22:06:15 +0700

    init

--- Laptopshop/src/main/java/vn/Laptopshop/HelloController.java
+++ Laptopshop/src/main/java/vn/Laptopshop/HelloController.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World HAOHV 2k5";
+        return "Hello World  test spring framework devtools";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access this page";
     }
 }
--- Laptopshop/src/main/java/vn/Laptopshop/LaptopshopApplication.java
+++ Laptopshop/src/main/java/vn/Laptopshop/LaptopshopApplication.java
@@ -2,8 +2,12 @@ package vn.Laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+})
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
