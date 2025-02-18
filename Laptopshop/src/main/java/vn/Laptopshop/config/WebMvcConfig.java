9bf6c4d5fa7920d9d0003569250d4ef36f53aa34
HaoHV2k5 <magicmath2k5@gmail.com>
2025-02-18 20:15:21 +0700

    #50

--- Laptopshop/src/main/java/vn/Laptopshop/config/WebMvcConfig.java
+++ Laptopshop/src/main/java/vn/Laptopshop/config/WebMvcConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -24,5 +26,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.viewResolver(viewResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/CSS/**")
+                .addResourceLocations("/resources/CSS/");
+
     }
 }
--- Laptopshop/src/main/webapp/WEB-INF/view/hello.jsp
+++ Laptopshop/src/main/webapp/WEB-INF/view/hello.jsp
@@ -7,11 +7,24 @@
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Document</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+                crossorigin="anonymous">
+
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+            <!-- <link rel="stylesheet" href="/CSS/demo.css"> -->
+
         </head>
 
         <body>
             <h1>${eric}</h1>
             <h2>${haone}</h2>
+            <button class="btn btn-warning">submit</button>
         </body>
 
         </html>
